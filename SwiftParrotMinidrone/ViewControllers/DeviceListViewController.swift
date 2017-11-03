2a506032392255f51ab8c6847f80138d674fe3f6
groovelab <nanba@groovelab.asia>
2017-11-03 12:26:59 +0900

    add sdk v3.13.0

---
+++ SwiftParrotMinidrone/ViewControllers/DeviceListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DeviceListViewController.swift
+//  SwiftParrotMinidrone
+//
+//  Created by ST14580 on 2017/11/03.
+//  Copyright © 2017年 ST14580. All rights reserved.
+//
+
+import UIKit
+
+class DeviceListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
